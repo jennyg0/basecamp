cef377fae08c91fc52970b2a72b37c047cbee213
Jen <60794961+jennyg0@users.noreply.github.com>
2023-11-22 18:21:10 +0100

    add week one basicmath exercise

---
+++ BasicMath.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract BasicMath {
+    
+    function adder(uint _a, uint _b) public pure returns (uint sum, bool error) {
+          unchecked {
+            sum = _a + _b;
+            if(sum < _a) { 
+                return (0, true);
+            }
+
+            return (sum, false);
+        }
+    }
+
+    function subtractor(uint _a, uint _b) public pure returns (uint difference, bool error) {
+       unchecked {
+            if (_b > _a) {
+                return (0, true);
+            }
+
+            difference = _a - _b;
+            return (difference, false);
+        }
+    }
+}
